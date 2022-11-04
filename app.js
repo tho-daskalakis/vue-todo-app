736ee5455e345f3b3c967d567040253bdc6f485e
Thomas <tho.daskalakis@gmail.com>
2022-11-04 18:56:48 +0200

    Implement remaining functionality
    Add a way to create and remove todos, mark them as done.
    
    Add a way to mark all todos as done simultaneously.

--- app.js
+++ app.js
@@ -5,11 +5,35 @@ createApp({
     return {
       title: 'Hello Vue!',
       todoText: '',
+      todoArr: [],
     };
   },
   methods: {
     addTodo() {
-      console.log();
+      // Check for empty input
+      if (this.todoText === '') {
+        alert('Input cannot be empty');
+        return;
+      }
+
+      this.todoArr.push({
+        text: this.todoText,
+        done: false,
+      });
+
+      // Clear input
+      this.todoText = '';
+    },
+
+    removeTodo(todo) {
+      const todoIndex = this.todoArr.indexOf(todo);
+      this.todoArr.splice(todoIndex, 1);
+    },
+
+    markAllAsDone() {
+      this.todoArr.forEach((todo) => {
+        todo.done = true;
+      });
     },
   },
 }).mount('#app');
